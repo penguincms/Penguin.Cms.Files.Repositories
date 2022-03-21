971bc16fa3bd941dc028ae325dee8440a444f921
MrJackSpade <loffredo.ap@gmail.com>
2022-03-20 21:34:57 -0700

    Autocommit
    Autocommit

--- DatabaseFileRepository.cs
+++ DatabaseFileRepository.cs
@@ -62,10 +62,7 @@ namespace Penguin.Cms.Files.Repositories
             base.DeleteRange(o);
         }
 
-        public override DatabaseFile Find(Guid guid)
-        {
-            return this.SecurityProvider.TryFind(base.Find(guid));
-        }
+        public override DatabaseFile Find(Guid guid) => this.SecurityProvider.TryFind(base.Find(guid));
 
         public DatabaseFile GetByFullName(string FullName)
         {
@@ -87,10 +84,7 @@ namespace Penguin.Cms.Files.Repositories
             return db;
         }
 
-        public List<DatabaseFile> GetByOwner(Guid OwnerGuid)
-        {
-            return this.Where(f => f.Owner == OwnerGuid).ToList();
-        }
+        public List<DatabaseFile> GetByOwner(Guid OwnerGuid) => this.Where(f => f.Owner == OwnerGuid).ToList();
 
         public List<DatabaseFile> GetByPath(string FilePath, bool Recursive = false)
         {
